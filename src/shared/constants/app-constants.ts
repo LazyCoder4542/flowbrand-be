1758a01741206a8f0893dfe6a0a857b3cde62a56
Adeola Fadare <adeolafadare8@gmail.com>
2026-05-10 09:16:51 +0100

    fix: hardcoded frontend url

--- src/shared/constants/app-constants.ts
+++ src/shared/constants/app-constants.ts
@@ -5,5 +5,5 @@ export const VALID_UPLOADS_MIME_TYPES = ['image/jpeg', 'image/png'];
 export const BASE_URL = 'https://staging.api-nestjs.boilerplate.hng.tech';
 export const PROFILE_PHOTO_UPLOADS = path.join(__dirname, '..', 'uploads');
 
-export const FRONTEND_URL = 'https://staging.api-nestjs.boilerplate.hng.tech';
-export const FRONTEND_RESET_PASSWORD = `${FRONTEND_URL}/forgot-password`;
+export const FRONTEND_URL = process.env.FRONTEND_URL ?? 'http://localhost:3000';
+export const FRONTEND_RESET_PASSWORD = `${FRONTEND_URL}/reset-password`;
