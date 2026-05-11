fd838f1decabb1ef25b71eb624a91f4e5638f057
John Ughiovhe <johnandrewjaw@gmail.com>
2026-05-11 10:00:42 +0100

    feat(auth): enhance session management and update JWT module imports

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import RegistrationController from './auth.controller';
 import authConfig from '@config/auth.config';
 import AuthenticationService from './auth.service';
@@ -12,13 +13,28 @@ import { GoogleStrategy } from '../strategies/google.strategy';
 import { LockoutService } from './lockout.service';
 import { SessionService } from './session.service';
 import type { StringValue } from 'ms';
+import { User } from '@modules/user/entities/user.entity';
 
 const expiry = authConfig().jwtExpiry;
 @Module({
   controllers: [RegistrationController],
-  providers: [AuthenticationService, GoogleStrategy],
+  providers: [
+    AuthenticationService,
+    GoogleStrategy,
+    {
+      provide: LockoutService,
+      useFactory: authMetadataRepository => new LockoutService(authMetadataRepository),
+      inject: [getRepositoryToken(AuthMetadata)],
+    },
+    {
+      provide: SessionService,
+      useFactory: userSessionRepository => new SessionService(userSessionRepository),
+      inject: [getRepositoryToken(UserSession)],
+    },
+  ],
   imports: [
     PassportModule,
+    TypeOrmModule.forFeature([User, UserSession, AuthMetadata]),
     RedisModule,
     EmailModule,
     JwtModule.register({
